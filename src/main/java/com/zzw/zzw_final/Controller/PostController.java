3f428233e5b77f7a570f607545ff9967b0bcdbdd
Jeeyeonn <qkrwldus3927@naver.com>
2022-09-19 11:45:41 +0900

    레시피 수정/삭제 틀 작성

--- src/main/java/com/zzw/zzw_final/Controller/PostController.java
+++ src/main/java/com/zzw/zzw_final/Controller/PostController.java
@@ -4,10 +4,7 @@ import com.zzw.zzw_final.Dto.Request.PostRecipeRequestDto;
 import com.zzw.zzw_final.Dto.Response.ResponseDto;
 import com.zzw.zzw_final.Service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,5 +20,16 @@ public class PostController {
     public ResponseDto<?> postRecipe(@RequestPart(value = "data") PostRecipeRequestDto requestDto, HttpServletRequest request,
                                      @RequestPart(value = "file") MultipartFile multipartFile){
         return postService.postRecipe(requestDto, request, multipartFile);
+    }
+
+    @PutMapping("/api/auth/post/{post_id}")
+    public ResponseDto<?> putRecipe(@RequestPart(value = "data") PostRecipeRequestDto requestDto, HttpServletRequest request,
+                                     @RequestPart(value = "file", required = false) MultipartFile multipartFile, @PathVariable Long post_id){
+        return postService.putRecipe(requestDto, request, multipartFile, post_id);
+    }
+
+    @DeleteMapping("/api/auth/post/{post_id}")
+    public ResponseDto<?> delteRecipe(HttpServletRequest request, @PathVariable Long post_id){
+        return postService.deleteRecipe(request, post_id);
     }
 }
