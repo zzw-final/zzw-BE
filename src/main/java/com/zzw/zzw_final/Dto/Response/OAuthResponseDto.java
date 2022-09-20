23823b671892b15a2dd6d177679c09a58f56e8f5
Jeeyeonn <qkrwldus3927@naver.com>
2022-09-20 12:33:20 +0900

    로그인 할 때 프론트에 추가 정보 전달

--- src/main/java/com/zzw/zzw_final/Dto/Response/OAuthResponseDto.java
+++ src/main/java/com/zzw/zzw_final/Dto/Response/OAuthResponseDto.java
@@ -12,11 +12,17 @@ public class OAuthResponseDto {
     private String accessToken;
     private String refreshToken;
     private String kakaoToken;
+    private Long userId;
+    private String nickname;
+    private String profile;
 
     public OAuthResponseDto(Member kakaoUser, TokenDto tokenDto, String kakaoToken) {
         this.email = kakaoUser.getEmail();
         this.accessToken = tokenDto.getAccessToken();
         this.refreshToken = tokenDto.getRefreshToken();
         this.kakaoToken = kakaoToken;
+        this.userId = kakaoUser.getId();
+        this.nickname = kakaoUser.getNickname();
+        this.profile = kakaoUser.getProfile();
     }
 }
