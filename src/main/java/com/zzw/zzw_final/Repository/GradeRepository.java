a232abeeacd9deb1369b74fb2518b6f96dff5966
Jeeyeonn <qkrwldus3927@naver.com>
2022-09-21 10:20:47 +0900

    마이페이지 유저정보 GET 구현

--- src/main/java/com/zzw/zzw_final/Repository/GradeRepository.java
+++ src/main/java/com/zzw/zzw_final/Repository/GradeRepository.java
@@ -1,7 +1,11 @@
 package com.zzw.zzw_final.Repository;
 
 import com.zzw.zzw_final.Dto.Entity.Grade;
+import com.zzw.zzw_final.Dto.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GradeRepository extends JpaRepository<Grade, Long> {
+    List<Grade> findAllByMember(Member member);
 }
--- src/main/java/com/zzw/zzw_final/Service/MypageService.java
+++ src/main/java/com/zzw/zzw_final/Service/MypageService.java
@@ -1,15 +1,21 @@
 package com.zzw.zzw_final.Service;
 
+import com.zzw.zzw_final.Dto.Entity.Follow;
 import com.zzw.zzw_final.Dto.Entity.Grade;
 import com.zzw.zzw_final.Dto.Entity.GradeList;
 import com.zzw.zzw_final.Dto.Entity.Member;
+import com.zzw.zzw_final.Dto.Response.GradeListResponseDto;
+import com.zzw.zzw_final.Dto.Response.MypageUserInfoResponseDto;
 import com.zzw.zzw_final.Dto.Response.ResponseDto;
+import com.zzw.zzw_final.Repository.FollowRepository;
 import com.zzw.zzw_final.Repository.GradeListRepository;
 import com.zzw.zzw_final.Repository.GradeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +24,25 @@ public class MypageService {
     private final MemberService memberService;
     private final GradeListRepository gradeListRepository;
     private final GradeRepository gradeRepository;
+    private final FollowRepository followRepository;
     public ResponseDto<?> getUserInfo(HttpServletRequest request) {
-        return ResponseDto.success("test");
+
+        ResponseDto<?> result = memberService.checkMember(request);
+        Member member = (Member) result.getData();
+
+        List<Follow> followerlist = followRepository.findAllByFollowerId(member.getId());
+        List<Follow> followList = followRepository.findAllByMember(member);
+
+        List<GradeListResponseDto> responseDtos = new ArrayList<>();
+        List<Grade> grades = gradeRepository.findAllByMember(member);
+        for(Grade grade : grades){
+            responseDtos.add(new GradeListResponseDto(grade.getGradeList()));
+        }
+
+        MypageUserInfoResponseDto responseDto = new MypageUserInfoResponseDto(member,
+                followList.size(), followerlist.size(), responseDtos);
+
+        return ResponseDto.success(responseDto);
     }
 
 
