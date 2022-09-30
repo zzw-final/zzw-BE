189ac11a92a7de0ca01770b4949dd2ab265c8db3
Jeeyeonn <qkrwldus3927@naver.com>
2022-09-30 15:51:36 +0900

    Update MemberServiceTest.java

--- src/test/java/com/zzw/zzw_final/Service/MemberServiceTest.java
+++ src/test/java/com/zzw/zzw_final/Service/MemberServiceTest.java
@@ -3,8 +3,10 @@ package com.zzw.zzw_final.Service;
 import com.zzw.zzw_final.Config.Jwt.TokenProvider;
 import com.zzw.zzw_final.Dto.Entity.Member;
 import com.zzw.zzw_final.Dto.Request.SignupRequestDto;
+import com.zzw.zzw_final.Dto.Response.ResponseDto;
 import com.zzw.zzw_final.Dto.TokenDto;
 import com.zzw.zzw_final.Repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.servlet.http.HttpServletResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
@@ -34,12 +38,12 @@ class MemberServiceTest {
         signupRequestDto.setNickname("jeeyeon");
         signupRequestDto.setEmail("good9712@nate.com");
 
+        //ResponseDto<?> responseDto = memberService.postUserNickname(,signupRequestDto);
+
         Member loginMember = new Member(signupRequestDto);
 
-        TokenDto tokenDto = tokenProvider.generateTokenDto(loginMember);
-
-
-
+        Assertions.assertEquals(loginMember.getNickname(), "jeeyeon");
+        Assertions.assertEquals(loginMember.getEmail(), "good9712@nate.com");
     }
 
 }
