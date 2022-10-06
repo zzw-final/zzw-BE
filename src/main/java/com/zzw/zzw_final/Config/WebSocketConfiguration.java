f12ba972383e4e17993d44cface653896a37390a
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-07 00:27:09 +0900

    채팅 된다 ,,, 드디ㅣ어 ,,,, ㅠㅠ

--- src/main/java/com/zzw/zzw_final/Config/WebSocketConfiguration.java
+++ src/main/java/com/zzw/zzw_final/Config/WebSocketConfiguration.java
@@ -6,28 +6,21 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//Stomp를 사용하기위해 선언하는 어노테이션
-@EnableWebSocketMessageBroker
+
 @Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/chat")
-                .setAllowedOriginPatterns("*");
-    }
-
-    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
-    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Client 에서 SEND 요청을 처리
-        registry.setApplicationDestinationPrefixes("/pub");
-
-        //해당 경로로 SimpleBroker를 등록.
-        // SimpleBroker는 해당하는 경로를 SUBSCRIBE하는 Client에게 메세지를 전달하는 간단한 작업을 수행
-        registry.enableSimpleBroker("/sub");
-        //enableStompBrokerRelay
-        //SimpleBroker의 기능과 외부 Message Broker( RabbitMQ, ActiveMQ 등 )에 메세지를 전달하는 기능을 가짐
+        registry.enableSimpleBroker("/sub"); // 구독 주소 : /sub/chat
+        registry.setApplicationDestinationPrefixes("/pub"); // 발행 주소  : /pub/chat
     }
-    
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/zzw")
+                .setAllowedOriginPatterns("*");  //"*://*"
+        //.withSockJS();
+    }
 }
--- src/main/java/com/zzw/zzw_final/Dto/Chat/ChatRequestDto.java
+++ src/main/java/com/zzw/zzw_final/Dto/Chat/ChatRequestDto.java
@@ -1,11 +1,13 @@
 package com.zzw.zzw_final.Dto.Chat;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 // 처음으로 입력받는 메세지
 @Getter
 @Setter
+@Data
 public class ChatRequestDto {
     //    private MessageType type; // 메시지 타입
     private Long roomId;
