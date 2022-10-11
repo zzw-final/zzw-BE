fd6259fbf88e8af8b4f38c98a8a22d8e5eb7b776
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-11 17:47:50 +0900

    testCode 틀 작성

---
+++ src/test/java/com/zzw/zzw_final/Service/GradeServiceTest.java
@@ -0,0 +1,20 @@
+package com.zzw.zzw_final.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GradeServiceTest {
+
+    @Test
+    void getMemberGrade() {
+    }
+
+    @Test
+    void updateMemberGrade() {
+    }
+
+    @Test
+    void postGrade() {
+    }
+}
---
+++ src/test/java/com/zzw/zzw_final/Service/ProfileServiceTest.java
@@ -0,0 +1,16 @@
+package com.zzw.zzw_final.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileServiceTest {
+
+    @Test
+    void updateMemberProfile() {
+    }
+
+    @Test
+    void getMemberProfile() {
+    }
+}
