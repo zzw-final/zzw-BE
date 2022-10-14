f5036ea7d8ffd6686450e07873ce75a8543aa447
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-14 17:23:17 +0900

    [TESTCODE] ChatService - exitChatRoom

--- src/test/java/com/zzw/zzw_final/Service/ChatServiceTest.java
+++ src/test/java/com/zzw/zzw_final/Service/ChatServiceTest.java
@@ -1,8 +1,12 @@
 package com.zzw.zzw_final.Service;
 
 import com.zzw.zzw_final.Config.Jwt.TokenProvider;
+import com.zzw.zzw_final.Dto.Entity.ChatMember;
+import com.zzw.zzw_final.Dto.Entity.ChatRoom;
+import com.zzw.zzw_final.Dto.Entity.Member;
 import com.zzw.zzw_final.Dto.Request.SignupRequestDto;
 import com.zzw.zzw_final.Repository.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,9 +27,9 @@ import static org.mockito.Mockito.when;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ChatServiceTest {
+
     @MockBean
     private TokenProvider tokenProvider;
-
     @Autowired
     private ChatRoomRepository chatRoomRepository;
     @Autowired
@@ -39,13 +43,41 @@ class ChatServiceTest {
     @Autowired
     private ChatReadRepository chatReadRepository;
 
+    HttpServletRequest request;
     @BeforeEach
     public void setup() {
-        
+        request = mock(HttpServletRequest.class);
+        when(request.getHeader("Authorization")).thenReturn("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnb29kOTcxMkBuYXRlLmNvbSIsImF1dGgiOiJST0xFX01FTUJFUiIsImV4cCI6MTY2NTU0NzE2N30.PQvOV9mzyNbtFPpY71XYlMjcjqpgN3HG2nzEChjMuo4");
+        when(request.getHeader("Refresh-Token")).thenReturn("eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjYwNjU1Njd9.vJtmCwajGZzdsq4W8JsPbL1dymVy7CkYpkA0dl296_g");
+        when(request.getHeader("oauth")).thenReturn("kakao");
     }
 
     @Test
     void exitChatRoom() {
+        //when
+        String token = request.getHeader("Authorization");
+        String oauth = request.getHeader("oauth");
+        when(tokenProvider.getUserEmail(token.substring(7))).thenReturn("good9712@nate.com");
+        String email = tokenProvider.getUserEmail(token.substring(7));
+        Member member = memberRepository.findMemberByEmailAndOauth(email, oauth);
+
+        ChatRoom null_chatRoom = chatRoomRepository.findChatRoomById(1L);
+        ChatRoom chatRoom = chatRoomRepository.findChatRoomById(3332L);
+
+        ChatMember chatMember = chatMemberRepository.findChatMemberByChatRoomAndMember(chatRoom, member);
+        ChatMember null_chatMember = chatMemberRepository.findChatMemberByChatRoomAndMember(null_chatRoom, member);
+
+
+        //then
+        Assertions.assertEquals(token, "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnb29kOTcxMkBuYXRlLmNvbSIsImF1dGgiOiJST0xFX01FTUJFUiIsImV4cCI6MTY2NTU0NzE2N30.PQvOV9mzyNbtFPpY71XYlMjcjqpgN3HG2nzEChjMuo4");
+        Assertions.assertEquals(oauth, "kakao");
+        Assertions.assertEquals(email, "good9712@nate.com");
+        Assertions.assertEquals(member.getEmail(), "good9712@nate.com");
+        Assertions.assertEquals(member.getOauth(), "kakao");
+        Assertions.assertNull(null_chatRoom);
+        Assertions.assertNotNull(chatRoom);
+        Assertions.assertNotNull(chatMember);
+        Assertions.assertNull(null_chatMember);
     }
 
     @Test
