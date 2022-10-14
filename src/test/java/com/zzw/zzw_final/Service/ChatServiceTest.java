2b084210725616fb2bce75f67c92da4b8b0a22e8
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-14 11:59:51 +0900

    Create ChatServiceTest.java

---
+++ src/test/java/com/zzw/zzw_final/Service/ChatServiceTest.java
@@ -0,0 +1,36 @@
+package com.zzw.zzw_final.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChatServiceTest {
+
+    @Test
+    void exitChatRoom() {
+    }
+
+    @Test
+    void sendMessage() {
+    }
+
+    @Test
+    void getMessage() {
+    }
+
+    @Test
+    void getChatRoom() {
+    }
+
+    @Test
+    void getUserChatList() {
+    }
+
+    @Test
+    void checkReadMessage() {
+    }
+
+    @Test
+    void isNewMessage() {
+    }
+}
