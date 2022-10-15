44ba45be4e2c1b9773ae3cb7789f94971fbf7efb
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-15 11:47:51 +0900

    레시피 작성 시 칭호 획득 여부를 체크

--- src/main/java/com/zzw/zzw_final/Repository/GradeRepository.java
+++ src/main/java/com/zzw/zzw_final/Repository/GradeRepository.java
@@ -1,6 +1,7 @@
 package com.zzw.zzw_final.Repository;
 
 import com.zzw.zzw_final.Dto.Entity.Grade;
+import com.zzw.zzw_final.Dto.Entity.GradeList;
 import com.zzw.zzw_final.Dto.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface GradeRepository extends JpaRepository<Grade, Long> {
     List<Grade> findAllByMember(Member member);
+    Grade findGradeByMemberAndGradeList(Member member, GradeList gradeList);
 }
