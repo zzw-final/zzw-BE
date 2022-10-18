e93a9135786e7898fe0a230cd6c0e55739912a92
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-18 15:16:07 +0900

    댓글 오름차순으로 정렬 변경함.

--- src/main/java/com/zzw/zzw_final/Repository/CommentRepository.java
+++ src/main/java/com/zzw/zzw_final/Repository/CommentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByPostOrderByCreatedAtDesc(Post post);
+    List<Comment> findAllByPostOrderByCreatedAtAsc(Post post);
     Comment findPostById(Long id);
     Comment findCommentById(Long id);
     List<Comment> findAllByMember(Member member);
