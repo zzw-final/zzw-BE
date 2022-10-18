bff527fd5fbe8001a9e9b2fc3b4a67f4a1454193
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-18 14:15:11 +0900

    Create PostServiceTest.java

---
+++ src/test/java/com/zzw/zzw_final/Service/PostServiceTest.java
@@ -0,0 +1,72 @@
+package com.zzw.zzw_final.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    void getBestRecipe() {
+    }
+
+    @Test
+    void getRecentRecipeInfinite() {
+    }
+
+    @Test
+    void getRecentRecipeTop10() {
+    }
+
+    @Test
+    void getBestTagList() {
+    }
+
+    @Test
+    void getResponsePostUserLike() {
+    }
+
+    @Test
+    void getIngredientByPost() {
+    }
+
+    @Test
+    void filterPostTitle() {
+    }
+
+    @Test
+    void filterPostNickname() {
+    }
+
+    @Test
+    void getAllTag() {
+    }
+
+    @Test
+    void filterPostTag() {
+    }
+
+    @Test
+    void isPostInTag() {
+    }
+
+    @Test
+    void postLike() {
+    }
+
+    @Test
+    void getUserPost() {
+    }
+
+    @Test
+    void getUserLikePosts() {
+    }
+
+    @Test
+    void getOtherUserPosts() {
+    }
+
+    @Test
+    void getFollowRecipe() {
+    }
+}
