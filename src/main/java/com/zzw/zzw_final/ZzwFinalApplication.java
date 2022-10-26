75f0dd65881072892dff5c9372cf3c1d87991a5d
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-26 10:50:09 +0900

    Git Action TEST

--- src/main/java/com/zzw/zzw_final/ZzwFinalApplication.java
+++ src/main/java/com/zzw/zzw_final/ZzwFinalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class ZzwFinalApplication {
+	// 자동배포 TEST
 	public static void main(String[] args) {
 		SpringApplication.run(ZzwFinalApplication.class, args);
 	}
